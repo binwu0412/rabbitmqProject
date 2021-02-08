1e631b5ce962c55c5ca1f1dfc3ba389b186e490a
binwu0412 <ding_60@hotmail.com>
2021-02-08 17:53:19 -0500

    Implemented interfaces and classes for domain.core

---
+++ RabbitmqMicoservices/RabbitmqMicroservices.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,23 @@
+﻿using RabbitmqMicroservices.Domain.Core.Commands;
+using RabbitmqMicroservices.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitmqMicroservices.Domain.Core.Bus
+{
+    interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+
+
+
+    }
+}
