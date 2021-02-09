b9deea54758190ab65d32948eee1f08165572d22
binwu0412 <ding_60@hotmail.com>
2021-02-08 22:35:38 -0500

    initial commit on dependency injection

---
+++ RabbitmqMicoservices/RabbitmqMicroservices.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RabbitmqMicroservices.Domain.Core.Bus;
+using RabbitmqMicroservices.Infra.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitmqMicroservices.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitmqBus>();
+        }
+    }
+}
